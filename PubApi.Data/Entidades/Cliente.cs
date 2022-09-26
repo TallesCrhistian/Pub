96572866c321aa90996570f4505bbe9525c460d3
talles arriel <tallsk8984@gmail.com>
2022-09-26 20:13:57 -0300

    Adicionar arquivos de projeto.

---
+++ PubApi.Data/Entidades/Cliente.cs
@@ -0,0 +1,11 @@
+﻿namespace PubApi.Data.Entidades
+{
+    public class Cliente
+    {
+        public int IdCliente { get; set; }
+        public bool Ativo { get; set; } = true;
+        public string Nome { get; set; }
+        public string SobreNome { get; set; }
+        public string Cpf { get; set; }
+    }
+}
---
+++ PubApi.Data/Interfaces/IClienteRepositorio.cs
@@ -0,0 +1,17 @@
+﻿using PubApi.Data.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PubApi.Data.Interfaces
+{
+    public interface IClienteRepositorio
+    {
+        Task<Cliente> Iserir(Cliente cliente);
+        Task<Cliente> Alterar(Cliente cliente);
+        Task<List<Cliente>> Listar(Cliente cliente);
+        Task<Cliente> ObterPorCodigo(int idCliente);
+    }
+}
